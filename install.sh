a22faf991da9c0ca4f36f688d41fbf6f516f166e
Job Cabral <jabjabj4@gmail.com>
2019-10-02 15:57:45 -0700

    Updated vim set up

--- install.sh
+++ install.sh
@@ -9,16 +9,12 @@ npm install
 echo ""
 
 cd ~
+mkdir .vimtmp
+
 rm .vimrc
 ln -s .vim/.vimrc .vimrc
 
 rm .editorconfig
 ln -s .vim/.editorconfig .editorconfig
 
-rm -rf .config/nvim
-mkdir .config/nvim
-echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after
-let &packpath = &runtimepath
-source ~/.vimrc" >> .config/nvim/init.vim
-
-echo "Successfully set up neovim."
+echo "Successfully set up vim."
